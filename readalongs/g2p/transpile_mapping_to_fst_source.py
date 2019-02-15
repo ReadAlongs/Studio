0a29330eeb14edc9e8d4cc0c44033311fe0a9bd8
Littell <Patrick.Littell@ds.nrc.ca>
2019-02-15 16:10:00 -0500

    Adding a transpiler between mapping files and XFST source

---
+++ readalongs/g2p/transpile_mapping_to_fst_source.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, division
+from io import open
+import argparse, json, itertools, logging
+
+def make_header_comments(mapping):
+    lines = []
+    lines.append( "#################################" )
+    lines.append( "#" )
+    lines.append( "# FST converting from %s-%s to %s-%s" % (mapping["in_metadata"]["lang"],
+                                                        mapping["in_metadata"]["orth"],
+                                                        mapping["out_metadata"]["lang"],
+                                                        mapping["out_metadata"]["orth"]) )
+    lines.append( "#" )
+    authors = " & ".join(mapping["authors"])
+    lines.append( "# Based on a mapping by %s " % (authors,) )
+    lines.append( "# created %s" % mapping["created"] )
+    lines.append( "# last modified %s" % mapping["last_modified"] )
+    lines.append( "#" )
+    lines.append( "#################################" )
+    lines.append( '' )
+    return lines
+
+def escape_xfst_specials(s):
+    return s.replace("-","%-").replace("0","%0").replace("|","%|").replace(":","%:")
+
+def compile_correspondences(mapping):
+    lines = []
+    corrs = [ (escape_xfst_specials(x["in"]), escape_xfst_specials(x["out"])) for x in mapping["map"]]
+    corrs = [ "{%s}:{%s}" % (x,y) for x,y in corrs ]
+    lines.append( "define LETTER [" + "|".join(corrs) + "] ;" )
+    lines.append( "define WORD LETTER* ; " )
+    lines.append( "push WORD" )
+    return lines
+
+def go(mapping_filename, output_filename):
+    with open(mapping_filename, "r", encoding="utf-8") as fin:
+        mapping = json.load(fin)
+    lines = "\n".join(make_header_comments(mapping) + compile_correspondences(mapping))
+    with open(output_filename, "w", encoding="utf-8") as fout:
+        fout.write(lines)
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(description='Create an inventory file from a mapping file')
+   parser.add_argument('mapping', type=str, help='Mapping filename')
+   parser.add_argument('output', type=str, help='Output inventory filename')
+   args = parser.parse_args()
+   go(args.mapping, args.output)
