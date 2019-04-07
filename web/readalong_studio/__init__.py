84654b2e168942ab8bd0c9f26f62fd538673df5f
Aidan Pine <aidanpine@shaw.ca>
2019-04-07 16:39:20 -0400

    Update __init__.py

--- web/readalong_studio/__init__.py
+++ web/readalong_studio/__init__.py
@@ -1,10 +1,6 @@
 ''' ReadAlong Studio App '''
 
 from flask import Flask
-from flask_talisman import Talisman, GOOGLE_CSP_POLICY
-import os
-from logging.config import dictConfig
-from logging import getLogger
 
 VERSION = '0.0.1'
 
