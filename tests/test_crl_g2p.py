6cbcbf53e5c15f25265c73082380911581518ebf
David Huggins Daines <dhdaines@gmail.com>
2019-04-07 14:40:22 -0400

    add test and sample test data

---
+++ tests/test_crl_g2p.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class TestNorthEastCreeG2P(TestCase):
+    pass
+
---
+++ tests/test_force_align.py
@@ -0,0 +1,11 @@
+"""
+Test force-alignment with PocketSphinx FSG search from Python API
+"""
+
+import unittest
+
+class TestForceAlignment(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
