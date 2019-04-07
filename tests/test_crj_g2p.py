194e20e849f49fdc70731d3d4a77eda4076bdc30
David Huggins Daines <dhdaines@gmail.com>
2019-04-07 14:51:39 -0400

    add tests for east cree g2p

---
+++ tests/test_crj_g2p.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+import logging
+
+import readalongs
+from readalongs.g2p.tokenize_xml import tokenize_xml
+from readalongs.g2p.add_ids_to_xml import add_ids
+from readalongs.g2p.convert_xml import convert_xml
+from lxml import etree
+
+import os, io
+
+SAMPLE = io.open(os.path.join(os.path.dirname(__file__),
+                              'test_crj_sample.xml')).read().strip()
+EXPECTED_TOKENIZED = io.open(
+    os.path.join(os.path.dirname(__file__),
+                 'test_crj_sample_tokenized.xml')).read().strip()
+EXPECTED_IDS = io.open(os.path.join(os.path.dirname(__file__),
+                                    'test_crj_sample_ids.xml')).read().strip()
+EXPECTED_CONVERTED = io.open(
+    os.path.join(os.path.dirname(__file__),
+                 'test_crj_sample_converted.xml')).read().strip()
+
+class TestSouthEastCreeG2P(TestCase):
+    mapping_dir = os.path.join(next(iter(readalongs.__path__)), 'lang')
+    def testTokenize(self):
+        xml = etree.fromstring(SAMPLE)
+        xml = tokenize_xml(xml, self.mapping_dir)
+        self.assertEqual(etree.tounicode(xml), EXPECTED_TOKENIZED)
+
+    def testAddIDs(self):
+        xml = etree.fromstring(SAMPLE)
+        xml = tokenize_xml(xml, self.mapping_dir)
+        xml = add_ids(xml)
+        self.assertEqual(etree.tounicode(xml), EXPECTED_IDS)
+
+    def testConvert(self):
+        xml = etree.fromstring(SAMPLE)
+        xml = tokenize_xml(xml, self.mapping_dir)
+        xml = add_ids(xml)
+        xml = convert_xml(self.mapping_dir, xml)
+        self.assertEqual(etree.tounicode(xml), EXPECTED_CONVERTED)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
