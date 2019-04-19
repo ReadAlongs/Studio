fc685c98f147bcff9fde7d5a03be55b5cb72bb15
David Huggins Daines <dhdaines@gmail.com>
2019-04-19 18:50:50 -0400

    udpate requirements

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     python_requires='>=3.6',
     version=readalongs.VERSION,
     long_description='ReadAlong Studio',
-    packages=find_packages(),
-    # packages=['readalong_studio'],
+    packages=find_packages(exclude=['test']),
     include_package_data=True,
     zip_safe=False,
     install_requires=['flask', 'flask_restful', 'flask_cors']
