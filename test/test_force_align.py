4f4c3294965c09564f9b8ad0c180e13b204f47b7
David Huggins Daines <dhdaines@gmail.com>
2019-04-21 12:44:24 -0400

    support XHTML output (for making ePubs)

--- test/test_force_align.py
+++ test/test_force_align.py
@@ -4,21 +4,26 @@ Test force-alignment with PocketSphinx FSG search from Python API
 
 import unittest
 import logging
+import tempfile
 import os
 from lxml import etree
 
-from readalongs.align import align_audio, create_input_xml
+from readalongs.align import align_audio, create_input_xml, convert_to_xhtml
+from readalongs.g2p.util import save_xml, load_txt
+
 
 class TestForceAlignment(unittest.TestCase):
     logging.basicConfig(level=logging.DEBUG)
     data_dir = os.path.dirname(__file__)
+
     def testAlign(self):
-        xml_path = os.path.join(self.data_dir, 'test_atj_sample.xml') 
+        xml_path = os.path.join(self.data_dir, 'test_atj_sample.xml')
         wav_path = os.path.join(self.data_dir, 'test_atj_sample.wav')
         results = align_audio(xml_path, wav_path, unit='w')
 
         # Verify that the same IDs are in the output
-        converted_path = os.path.join(self.data_dir, 'test_atj_sample_converted.xml') 
+        converted_path = os.path.join(
+            self.data_dir, 'test_atj_sample_converted.xml')
         xml = etree.parse(converted_path).getroot()
         words = results['words']
         xml_words = xml.xpath(".//w")
@@ -27,19 +32,37 @@ class TestForceAlignment(unittest.TestCase):
             self.assertEqual(xw.attrib['id'], w['id'])
 
     def testAlignText(self):
-        txt_path = os.path.join(self.data_dir, 'test_atj_sample.txt') 
+        txt_path = os.path.join(self.data_dir, 'test_atj_sample.txt')
         wav_path = os.path.join(self.data_dir, 'test_atj_sample.wav')
         tempfile = create_input_xml(txt_path, 'atj')
         results = align_audio(tempfile.name, wav_path, unit='w')
 
         # Verify that the same IDs are in the output
-        converted_path = os.path.join(self.data_dir, 'test_atj_sample_converted.xml') 
+        converted_path = os.path.join(
+            self.data_dir, 'test_atj_sample_converted.xml')
         xml = etree.parse(converted_path).getroot()
         words = results['words']
         xml_words = xml.xpath(".//w")
         self.assertEqual(len(words), len(xml_words))
         for w, xw in zip(words, xml_words):
             self.assertEqual(xw.attrib['id'], w['id'])
+
+
+class TestXHTML(unittest.TestCase):
+    data_dir = os.path.dirname(__file__)
+
+    def testConvert(self):
+        xml_path = os.path.join(self.data_dir, 'test_atj_sample_tokenized.xml')
+        xml = etree.parse(xml_path).getroot()
+        convert_to_xhtml(xml)
+        with tempfile.NamedTemporaryFile(suffix='.xml') as tf:
+            save_xml(tf.name, xml)
+            txt = load_txt(tf.name)
+            self.assertEqual(txt,
+                             load_txt(
+                                 os.path.join(self.data_dir,
+                                              'test_atj_sample_tokenized.xhtml')))
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
