4ab97bde506f380885338586d7ad0741d1ea6ac0
David Huggins Daines <dhdaines@gmail.com>
2019-04-23 21:32:12 -0400

    remove old scripts as they are fully replaced by the code now

--- atikamekw-scripts/run_pocketsphinx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pocketsphinx_batch -ctl fileids -fsgctl fileids -fsgext .fsg -cepdir . -dict ps_arpabet.dict -hypseg hypseg -backtrace yes -pbeam 1e-80 -wbeam 1e-80 -beam 1e-80
--- atikamekw-scripts/run_sphinx_fe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Must be a better way to find this!
-PSDIR=$(dirname $(which sphinx_fe))/../share/pocketsphinx
-sphinx_fe -c fileids -mswav yes -remove_noise no -remove_silence no -ei wav -eo mfc \
-	  -argfile $PSDIR/model/en-us/en-us/feat.params
