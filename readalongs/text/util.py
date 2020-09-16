39648a3b31cdb6db81f7ac32fb20eba6cd9e9450
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2020-09-16 11:10:53 -0400

    chore: remove dead code is_do_not_align_recursive

--- readalongs/text/util.py
+++ readalongs/text/util.py
@@ -71,16 +71,6 @@ def is_do_not_align(element):
     return dna == "true" or dna == "True" or dna == "TRUE" or dna == "1"
 
 
-def is_do_not_align_recursive(element):
-    if "do-not-align" in element.attrib:
-        dna = element.attrib["do-not-align"]
-        return dna == "true" or dna == "True" or dna == "TRUE" or dna == "1"
-    elif element.getparent() is not None:
-        return is_do_not_align_recursive(element.getparent())
-    else:
-        return False
-
-
 def set_lang_attrib(element, lang):
     nsmap = element.nsmap if hasattr(element, "nsmap") else element.getroot().nsmap
     xml_ns = nsmap.get("xml", "http://www.w3.org/XML/1998/namespace")
