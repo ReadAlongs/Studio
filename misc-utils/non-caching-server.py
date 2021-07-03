dc1657965cce69f0a628d4c3df7ca9f7440ee723
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-07-03 07:47:56 -0400

    docs: complete the license description of non-caching-server.py

--- misc-utils/non-caching-server.py
+++ misc-utils/non-caching-server.py
@@ -2,13 +2,65 @@
 
 # This script is copied and modified from
 # https://github.com/python/cpython/blob/3.9/Lib/http/server.py
-# The original script is Copyright (c) Python Software Foundation and licensed
-# under the Python Software Foundation License Version 2.
-# See https://github.com/python/cpython/blob/main/LICENSE for the full details.
+# The original script is Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+# 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
+# 2021 Python Software Foundation; All Rights Reserved
+# and licensed under the Python Software Foundation License Version 2
+# https://github.com/python/cpython/blob/main/LICENSE
+#
+# PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+# --------------------------------------------
+#
+# 1. This LICENSE AGREEMENT is between the Python Software Foundation
+# ("PSF"), and the Individual or Organization ("Licensee") accessing and
+# otherwise using this software ("Python") in source or binary form and
+# its associated documentation.
+#
+# 2. Subject to the terms and conditions of this License Agreement, PSF hereby
+# grants Licensee a nonexclusive, royalty-free, world-wide license to reproduce,
+# analyze, test, perform and/or display publicly, prepare derivative works,
+# distribute, and otherwise use Python alone or in any derivative version,
+# provided, however, that PSF's License Agreement and PSF's notice of copyright,
+# i.e., "Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+# 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Python Software Foundation;
+# All Rights Reserved" are retained in Python alone or in any derivative version
+# prepared by Licensee.
+#
+# 3. In the event Licensee prepares a derivative work that is based on
+# or incorporates Python or any part thereof, and wants to make
+# the derivative work available to others as provided herein, then
+# Licensee hereby agrees to include in any such work a brief summary of
+# the changes made to Python.
+#
+# 4. PSF is making Python available to Licensee on an "AS IS"
+# basis.  PSF MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR
+# IMPLIED.  BY WAY OF EXAMPLE, BUT NOT LIMITATION, PSF MAKES NO AND
+# DISCLAIMS ANY REPRESENTATION OR WARRANTY OF MERCHANTABILITY OR FITNESS
+# FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF PYTHON WILL NOT
+# INFRINGE ANY THIRD PARTY RIGHTS.
+#
+# 5. PSF SHALL NOT BE LIABLE TO LICENSEE OR ANY OTHER USERS OF PYTHON
+# FOR ANY INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES OR LOSS AS
+# A RESULT OF MODIFYING, DISTRIBUTING, OR OTHERWISE USING PYTHON,
+# OR ANY DERIVATIVE THEREOF, EVEN IF ADVISED OF THE POSSIBILITY THEREOF.
+#
+# 6. This License Agreement will automatically terminate upon a material
+# breach of its terms and conditions.
+#
+# 7. Nothing in this License Agreement shall be deemed to create any
+# relationship of agency, partnership, or joint venture between PSF and
+# Licensee.  This License Agreement does not grant permission to use PSF
+# trademarks or trade name in a trademark sense to endorse or promote
+# products or services of Licensee, or any third party.
+#
+# 8. By copying, installing or otherwise using Python, Licensee
+# agrees to be bound by the terms and conditions of this License
+# Agreement.
+
 
 # The modification made at National Research Council Canada are released under
 # the MIT License.
-
+#
 # MIT License
 #
 # Copyright (c) 2021, National Research Council Canada (only for the NRC
@@ -39,6 +91,15 @@
 #   to all requests.
 #   This is to work around an issue on Mac computers where sometimes even a hard
 #   refresh will not fetch manually updated pages.
+#
+#   Running this script in a root web site folder is equivalent to running
+#       python3 -m http.server
+#   in that folder, except that pages won't get cached.
+#
+# - Eric Joanis, 2021:
+#   - document the license and modification history of this script.
+#   - reformat with black and isort, and quiet flake8 warnings, so I can pass CI
+#     in the ReadAlongs/Studio repo.
 
 import contextlib
 import os
