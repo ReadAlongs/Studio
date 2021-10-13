27517527d98e7d7a7fe0f92f7dfa5eae5f23356f
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-10-13 16:00:53 -0400

    style: make test_package_urls.py executable and add fn docstring

--- test/test_package_urls.py
+++ test/test_package_urls.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+from unittest import main
+
 import requests
 from basic_test_case import BasicTestCase
 
@@ -6,6 +10,11 @@ from readalongs.text.make_package import FONTS_BUNDLE_URL, JS_BUNDLE_URL
 
 class TestPackageURLs(BasicTestCase):
     def test_urls(self):
+        """Test the links that our package functionality depends on"""
         for endpoint in [FONTS_BUNDLE_URL, JS_BUNDLE_URL]:
             res = requests.get(endpoint)
             self.assertEqual(res.status_code, 200)
+
+
+if __name__ == "__name__":
+    main()
