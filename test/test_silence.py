a75dca271bc8b9b371c5b5de989df99f5db9a5a6
roedoejet <aidanpine@shaw.ca>
2021-10-18 14:13:39 -0700

    refactor(silence): change syntax for adding silence and allow output to variety of audio formats

--- test/test_silence.py
+++ test/test_silence.py
@@ -33,19 +33,26 @@ class TestSilence(BasicTestCase):
             ],
         )
         self.assertEqual(results.exit_code, 0)
-        self.assertTrue(os.path.exists(os.path.join(output, "silence.wav")))
+        self.assertTrue(os.path.exists(os.path.join(output, "silence.m4a")))
         # test silence spans in output xml
         with open(os.path.join(output, "silence.xml"), "rb") as f:
             xml_bytes = f.read()
         root = etree.fromstring(xml_bytes)
-        silence_spans = root.xpath("//*[@silence]")
+        silence_spans = root.xpath("//silence")
         self.assertEqual(len(silence_spans), 3)
         # test audio has correct amount of silence added
         original_audio = AudioSegment.from_file(
             os.path.join(self.data_dir, "ej-fra.m4a")
         )
-        new_audio = AudioSegment.from_wav(os.path.join(output, "silence.wav"))
-        self.assertEqual(len(new_audio) - len(original_audio), 2882)
+        new_audio = AudioSegment.from_file(
+            os.path.join(output, "silence.m4a"), format="m4a"
+        )
+        self.assertAlmostEqual(
+            len(new_audio) - len(original_audio),
+            2882,
+            msg="silence-added audio file is more than 50ms shorter or longer",
+            delta=50,
+        )
 
 
 if __name__ == "__main__":
