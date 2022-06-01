566753b0cb60a140d987d598bb0246523f4bb1d9
David Huggins-Daines <dhdaines@gmail.com>
2022-06-01 10:29:27 -0400

    test: fix missing coverage of noisedict code

--- test/test_force_align.py
+++ test/test_force_align.py
@@ -5,10 +5,13 @@ Test force-alignment with SoundsSwallower FSG search from Python API
 """
 
 import os
+import shutil
 import unittest
+from tempfile import TemporaryDirectory
 
 from basic_test_case import BasicTestCase
 from lxml import etree
+from soundswallower import get_model_path
 
 from readalongs.align import align_audio, convert_to_xhtml, create_input_tei
 from readalongs.log import LOGGER
@@ -52,6 +55,35 @@ class TestForceAlignment(BasicTestCase):
         for w, xw in zip(words, xml_words):
             self.assertEqual(xw.attrib["id"], w["id"])
 
+    def test_align_switch_am(self):
+        """Alignment test case with an alternate acoustic model and custom
+        noise dictionary."""
+        xml_path = os.path.join(self.data_dir, "ej-fra.xml")
+        wav_path = os.path.join(self.data_dir, "ej-fra.m4a")
+        # Try with some extra stuff in the noisedict
+        with TemporaryDirectory(prefix="readalongs_am_") as tempdir:
+            custom_am_path = os.path.join(tempdir, "en-us")
+            shutil.copytree(get_model_path("en-us"), custom_am_path)
+            with open(os.path.join(custom_am_path, "noisedict"), "at") as fh:
+                fh.write(";; here is a comment\n")
+                fh.write("[BOGUS] SIL\n")
+            results = align_audio(
+                xml_path, wav_path, unit="w", config={"acoustic_model": custom_am_path}
+            )
+            # Try with no noisedict
+            os.remove(os.path.join(custom_am_path, "noisedict"))
+            results = align_audio(
+                xml_path, wav_path, unit="w", config={"acoustic_model": custom_am_path}
+            )
+        # Verify that the same IDs are in the output
+        converted_path = os.path.join(self.data_dir, "ej-fra-converted.xml")
+        xml = etree.parse(converted_path).getroot()
+        words = results["words"]
+        xml_words = xml.xpath(".//w")
+        self.assertEqual(len(words), len(xml_words))
+        for w, xw in zip(words, xml_words):
+            self.assertEqual(xw.attrib["id"], w["id"])
+
 
 class TestXHTML(BasicTestCase):
     """Test converting the output to xhtml"""
@@ -66,7 +98,8 @@ class TestXHTML(BasicTestCase):
             txt = load_txt(tf.name)
             self.maxDiff = None
             self.assertEqual(
-                txt, load_txt(os.path.join(self.data_dir, "ej-fra-converted.xhtml")),
+                txt,
+                load_txt(os.path.join(self.data_dir, "ej-fra-converted.xhtml")),
             )
 
 
