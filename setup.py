ec00b4fb868f6308361ebd4cd7e55ab3c64f3adf
Aidan Pine <hello@aidanpine.ca>
2022-11-14 18:57:45 +0000

    docs(setup): improve pypi
    add readme as long description and add link to github

--- setup.py
+++ setup.py
@@ -12,8 +12,13 @@ VERSION = readalongs.VERSION + "." + build_no
 with open(version_path, "w", newline="\n", encoding="utf-8") as f:
     print(f'__version__ = "{VERSION}"', file=f)
 
-with open("requirements.min.txt") as f:
+with open("requirements.min.txt", encoding="utf8") as f:
     required = f.read().splitlines()
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, "README.md"), encoding="utf8") as f:
+    long_description = f.read()
 
 setup(
     name="readalongs",
@@ -21,7 +26,9 @@ setup(
     python_requires=">=3.7",
     version=VERSION,
     description="ReadAlong Studio",
-    long_description="ReadAlong Studio, Audiobook alignment for Indigenous languages",
+    url="https://github.com/ReadAlongs/Studio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     platform=["any"],
     packages=find_packages(exclude=["test"]),
     include_package_data=True,
