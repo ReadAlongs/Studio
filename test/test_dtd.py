507a2b0f66226e13d06ab26365f766954a61fd7d
Delasie Torkornoo <Delasie.Torkornoo@nrc-cnrc.gc.ca>
2024-07-15 10:46:32 -0400

    test: added backwards compatiblity test for DTD

--- test/test_dtd.py
+++ test/test_dtd.py
@@ -8,6 +8,8 @@ from unittest import TestCase, main
 
 from lxml import etree
 
+from readalongs.text.util import load_xml
+
 DTDPATH = os.path.join(
     dirname(__file__), "..", "readalongs", "static", "read-along-1.1.dtd"
 )
@@ -46,17 +48,61 @@ class TestDTD(TestCase):
             path = os.path.join(dirname(__file__), "data", name)
             # DTD is text, XML is binary... okay
             with open(path, "rb") as infh:
-                parsed = etree.parse(infh)
-                self.assertTrue(self.dtd.validate(parsed), f"{name} does not validate")
+                try:
+                    parsed = load_xml(infh)
+                    self.assertTrue(
+                        self.dtd.validate(parsed), f"{name} does not validate"
+                    )
+                except etree.ParseError as e:
+                    self.fail("Error parsing XML input file %s: %s." % (path, e))
 
     def test_invalid_inputs(self):
         for name in INVALID_RAS:
             path = os.path.join(dirname(__file__), "data", name)
             with open(path, "rb") as infh:
-                parsed = etree.parse(infh)
-                self.assertFalse(
-                    self.dtd.validate(parsed), f"{name} validates but shouldn't"
-                )
+                try:
+                    parsed = load_xml(infh)
+                    self.assertFalse(
+                        self.dtd.validate(parsed), f"{name} validates but shouldn't"
+                    )
+                except etree.ParseError as e:
+                    self.fail("Error parsing XML input file %s: %s." % (path, e))
+
+    def test_backwards_compatibility(self):
+        # the DTD needs to be backwards compatible as long as the major version does not change
+        versions = ["ras-dtd-1.0.readalong", "ras-dtd-1.1.readalong"]
+        for name in versions:
+            path = os.path.join(dirname(__file__), "data", name.strip())
+            # DTD is text, XML is binary... okay
+            with open(path, "rb") as infh:
+                try:
+                    parsed = load_xml(infh)
+                    self.assertTrue(
+                        self.dtd.validate(parsed), f"{name} does not validate"
+                    )
+                except etree.ParseError as e:
+                    self.fail("Error parsing XML input file %s: %s." % (path, e))
+
+        # test that previous DTD fails current version
+        # test DTD 1.0 with format 1.1
+        with open(
+            os.path.join(
+                dirname(__file__), "..", "readalongs", "static", "read-along-1.0.dtd"
+            ),
+            "rt",
+        ) as dtdFile:
+            dtd = etree.DTD(dtdFile)
+            with open(
+                os.path.join(dirname(__file__), "data", versions[1]), "rb"
+            ) as rasFile:
+                try:
+                    parsed = load_xml(rasFile)
+                    self.assertFalse(
+                        dtd.validate(parsed),
+                        f"{versions[1]} validates with 1.0 but shouldn't",
+                    )
+                except etree.ParseError as e:
+                    self.fail("Error parsing XML input file %s: %s." % (rasFile, e))
 
 
 if __name__ == "__main__":
