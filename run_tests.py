4a165bf800facdbb9b0b87b1a9692613a66cbe4e
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-04-23 11:51:08 -0400

    refactor(tests): convert run_tests to use pytest fully

--- run_tests.py
+++ run_tests.py
@@ -4,154 +4,121 @@
 Top-level runner for out test suites
 
 Invoke as
-   ./run.py [suite]
+   ./run_tests.py [suite]
 where [suite] can be one of:
    all: run everything, by searching the directory for all test suite files
-   prod: synonym for all
-   dev: run the standard development test suite - this is what we do in CI
+   dev: now a synonym for all (used to exclude some expensive tests)
+   api: run only the API-related tests
+   cli: run only the CLI-related tests
    e2e: run the end-to-end tests
-   other: run the other tests
 """
 
 import argparse
-import os
-import re
+import io
 import sys
-from unittest import TestLoader, TestSuite, TextTestRunner
+from contextlib import redirect_stdout
+from pathlib import Path
+from typing import List, Optional
+
+import pytest
 
 from readalongs.log import LOGGER
-from tests.test_align_cli import TestAlignCli
-from tests.test_anchors import TestAnchors
-from tests.test_api import TestAlignApi
-from tests.test_audio import TestAudio
-from tests.test_config import TestConfig
-from tests.test_dna_text import TestDNAText
-from tests.test_dna_utils import TestDNAUtils
-from tests.test_dtd import TestDTD
-from tests.test_force_align import TestForceAlignment, TestXHTML
-from tests.test_g2p_cli import TestG2pCli
-from tests.test_make_xml_cli import TestMakeXMLCli
-from tests.test_misc import TestMisc
-from tests.test_package_urls import TestPackageURLs
-from tests.test_silence import TestSilence
-from tests.test_smil import TestSmilUtilities
-from tests.test_temp_file import TestTempFile
-from tests.test_tokenize_cli import TestTokenizeCli
-from tests.test_tokenize_xml import TestTokenizer
-from tests.test_web_api import TestWebApi
 
-LOADER = TestLoader()
+SUITES = {
+    "all": [],  # relies on discovery to collect all tests
+    "dev": [],  # synonym for all
+    "api": ["test_web_api", "test_api"],
+    "cli": ["test_align_cli", "test_g2p_cli", "test_make_xml_cli", "test_tokenize_cli"],
+    "e2e": ["test_force_align", "test_align_cli"],
+}
 
-e2e_tests = [
-    LOADER.loadTestsFromTestCase(test) for test in (TestForceAlignment, TestXHTML)
-]
-
-api_tests = [
-    LOADER.loadTestsFromTestCase(test) for test in [TestWebApi]
-]  # TODO: add some load testing with https://locust.io/
-
-other_tests = [
-    LOADER.loadTestsFromTestCase(test)
-    for test in [
-        TestAnchors,
-        TestConfig,
-        TestDNAText,
-        TestDNAUtils,
-        TestTokenizer,
-        TestTokenizeCli,
-        TestTempFile,
-        TestMakeXMLCli,
-        TestAudio,
-        TestAlignCli,
-        TestAlignApi,
-        TestG2pCli,
-        TestMisc,
-        TestSilence,
-        TestSmilUtilities,
-        TestPackageURLs,
-        TestWebApi,
-        TestDTD,
-    ]
-]
+# TODO: add some load testing with https://locust.io/
 
 
-def list_tests(suite: TestSuite):
-    for subsuite in suite:
-        for match in re.finditer(r"tests=\[([^][]+)\]>", str(subsuite)):
-            yield from match[1].split(", ")
+class PytestCollectorPlugin:
+    def __init__(self):
+        self.collected = []
+
+    def pytest_collection_modifyitems(self, session, config, items):
+        self.collected.extend([item.nodeid for item in items])
 
 
-def describe_suite(suite: TestSuite):
-    full_suite = LOADER.discover(os.path.dirname(__file__))
-    full_list = list(list_tests(full_suite))
-    requested_list = list(list_tests(suite))
+def list_tests(suite: List[str]):
+    plugin = PytestCollectorPlugin()
+    pytest_args = ["--collect-only", *suite, "-q"]
+    if sys.version_info >= (3, 10):
+        with redirect_stdout(io.StringIO()):  # broken with py 3.8/3.9...
+            pytest.main(pytest_args, plugins=[plugin])
+    else:
+        pytest.main(pytest_args, plugins=[plugin])
+    # print("===========\n", o.getvalue(), "\n================")
+    return plugin.collected
+
+
+def describe_suite(suite_name, suite_filenames: List[str]):
+    full_list = list_tests([])
+    requested_list = list_tests(suite_filenames)
     requested_set = set(requested_list)
-    print("Test suite includes:", *sorted(requested_list), sep="\n")
+    print(f"Test suite '{suite_name}' includes:", *sorted(requested_list), sep="\n")
     print(
-        "\nTest suite excludes:",
+        f"\nTest suite '{suite_name}' excludes:",
         *sorted(test for test in full_list if test not in requested_set),
         sep="\n",
     )
+    print(
+        "\nTotal test cases",
+        f"found: {len(full_list)};",
+        f"included: {len(requested_list)};",
+        f"excluded: {len(full_list) - len(requested_list)}.",
+    )
 
 
-SUITES = ["all", "dev", "e2e", "prod", "api", "other"]
-
-
-def run_tests(suite: str, describe: bool = False, verbosity=3) -> bool:
+def run_tests(suite: Optional[str], describe=False, verbose=False) -> bool:
     """Run the specified test suite.
 
     Args:
         suite: one of SUITES, "dev" if the empty string
         describe: if True, list all the test cases instead of running them.
 
-    Returns: True iff success
+    Returns: Bool: True iff success
     """
 
     if not suite:
-        LOGGER.info("No test suite specified, defaulting to dev.")
+        LOGGER.info("No test suite specified, defaulting to dev, which runs all tests.")
         suite = "dev"
 
-    if suite == "e2e":
-        test_suite = TestSuite(e2e_tests)
-    elif suite == "api":
-        test_suite = TestSuite(api_tests)
-    elif suite == "dev":
-        test_suite = TestSuite(other_tests + e2e_tests)
-    elif suite in ("prod", "all"):
-        test_suite = LOADER.discover(os.path.dirname(__file__))
-    elif suite == "other":
-        test_suite = TestSuite(other_tests)
-    else:
-        LOGGER.error(
-            "Sorry, you need to select a Test Suite to run, one of: " + " ".join(SUITES)
-        )
+    if suite not in SUITES:
+        LOGGER.error("Please specify a test suite to run among: " + ", ".join(SUITES))
         return False
 
+    test_suite = SUITES[suite]
+    tests_dir = Path(__file__).parent / "tests"
+    test_suite_filenames = [str(tests_dir / f"{file}.py") for file in test_suite]
     if describe:
-        describe_suite(test_suite)
+        describe_suite(suite, test_suite_filenames)
         return True
     else:
-        runner = TextTestRunner(verbosity=verbosity)
-        success = runner.run(test_suite).wasSuccessful()
-        if not success:
-            LOGGER.error("Some tests failed. Please see log above.")
-        return success
+        pytest_args = ["--verbose"] if verbose else []
+        return 0 == pytest.main([*test_suite_filenames, *pytest_args])
 
 
-if __name__ == "__main__":
+def main() -> None:
     parser = argparse.ArgumentParser(description="Run ReadAlongs/Studio test suites.")
-    parser.add_argument("--quiet", "-q", action="store_true", help="reduce output")
+    parser.add_argument("--verbose", "-v", action="store_true", help="verbose output")
     parser.add_argument(
         "--describe", action="store_true", help="describe the selected test suite"
     )
     parser.add_argument(
         "suite",
         nargs="?",
-        default="dev",
         help="the test suite to run [dev]",
         choices=SUITES,
     )
     args = parser.parse_args()
-    result = run_tests(args.suite, args.describe, 1 if args.quiet else 3)
+    result = run_tests(args.suite, args.describe, args.verbose)
     if not result:
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
