1e2f559e3623c97ea4e76003f01433d852e60b34
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-09-10 13:51:29 -0400

    fix(tests): correct test error flagged by AP in review

--- tests/test_anchors.py
+++ tests/test_anchors.py
@@ -34,7 +34,7 @@ class TestAnchors(BasicTestCase):
         assert words[0]["end"] <= 1.62
         assert words[1]["start"] >= 1.62
         assert words[8]["end"] <= 3.81
-        assert words[9]["start"] >= 3.81
+        assert words[9]["start"] >= 3.82
         assert words[21]["end"] <= 6.74
         assert words[22]["start"] >= 6.74
 
